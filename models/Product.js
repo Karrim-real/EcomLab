54424446918aaf7f009c6d2c91eaed8c8de65215
Akinyemi Taiwo <karrim4real@gmail.com>
2025-05-05 00:22:04 +0100

    Sql added

--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,6 @@
 
-const db = require('../util/database');
+const Sequelize = require('sequelize');
+const sequelize = require('../util/database');
 class Product {
     constructor(products){
         this.products = products
--- util/database.js
+++ util/database.js
@@ -1,9 +1,7 @@
-const mysql = require('mysql2');
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'ecommerce_lab',
-    password: ''
-});
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('ecommerce-lab', 'root', '', {
+    dialect: 'mysql',
+    host: 'localhost'
+})
 
-module.exports = pool.promise();
+module.exports = sequelize
